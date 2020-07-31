c223d807a66f71f07831d4c64e36f122bb555e1a
AdewaleOdunlami <adewaleodunlami@yahoo.com>
2020-07-30 23:21:54 -0400

    used accessor

--- ProcessCoordinates/BusinessLogic/ProcessAddress.cs
+++ ProcessCoordinates/BusinessLogic/ProcessAddress.cs
@@ -5,7 +5,8 @@ namespace PersonnelMapping.ProcessCoordinates.BusinessLogic
 {
     public static class ProcessAddress
     {
-        public const string NoStateCode = "Please provide a valid state code.";
+        //public static string NoStateCode = "Please provide a valid state code.";
+        public static string NoStateCode => "Please provide a valid state code.";
         public static void UpdateAddressCoordinates(Address address, string response)
         {
             var coordinates = JsonConvert.DeserializeObject<RootObject>(response).Features[0].Geometry.Coordinates;
